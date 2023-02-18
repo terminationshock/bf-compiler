6f3a2a7b1b2014852a7a8fe2c31b767e645b2c26
Tobias Melson <code@tobias-melson.de>
2023-02-18 14:53:52 +0100

    Refactored optimization routine

--- src/optimize.go
+++ src/optimize.go
@@ -6,20 +6,25 @@ import (
 )
 
 func Optimize(commands []*Command, verbose bool) []*Command {
-	cnt := -1
+	if verbose {
+		fmt.Println("Optimization report:")
+	}
+
+	optimized := optimizeUselessLoops(commands, verbose)
+	optimized = optimizeDuplicatedCommands(optimized, "><+-", verbose)
+	optimized = optimizeMultiplyLoops(optimized, verbose)
+	optimized = optimizeDuplicatedCommands(optimized, "]", verbose)
+
+	return optimized
+}
+
+func optimizeUselessLoops(commands []*Command, verbose bool) []*Command {
 	optimized := []*Command{}
 	i := 0
 	initialValueChanged := false
 	skippedLoop := 0
-	currentLoop := []*Command{}
-	currentLoopBegin := 0
-
-	if verbose {
-		fmt.Println("Optimization report:")
-	}
 	for i < len(commands) {
 		if skippedLoop > 0 {
-			// Skip all commands within the first useless loop(s)
 			if commands[i].String == "[" {
 				skippedLoop++
 			} else if commands[i].String == "]" {
@@ -27,50 +32,72 @@ func Optimize(commands []*Command, verbose bool) []*Command {
 			}
 			i++
 		} else if !initialValueChanged && commands[i].String == "[" {
-			// Whether to skip a useless loop
 			skippedLoop = 1
 			report(verbose, commands[i], "Skipping unreachable loop")
 			i++
-		} else if cnt >= 0 && optimized[cnt].String == commands[i].String && strings.Contains("><+-]", commands[i].String) {
-			// Increase counter for duplicated symbols
-			optimized[cnt].Count++
-			i++
 		} else if i < len(commands) - 1 && commands[i].String == "[" && commands[i+1].String == "]" {
-			// Skip empty loop
 			report(verbose, commands[i], "Skipping empty loop []")
 			i += 2
 		} else {
+			optimized = append(optimized, commands[i])
+			if !initialValueChanged {
+				initialValueChanged = strings.Contains("+-,", commands[i].String)
+			}
+			i++
+		}
+	}
+	return optimized
+}
+
+func optimizeDuplicatedCommands(commands []*Command, pattern string, verbose bool) []*Command {
+	optimized := []*Command{}
+	cnt := -1
+	i := 0
+	for i < len(commands) {
+		if cnt >= 0 && optimized[cnt].String == commands[i].String && strings.Contains(pattern, commands[i].String) {
+			optimized[cnt].Count++
+		} else {
 			if verbose {
 				n := len(optimized) - 1
-				if n >= 0 && optimized[n].Count > 1 {
+				if n >= 0 && optimized[n].Count > 1 && strings.Contains(pattern, optimized[n].String) {
 					report(verbose, optimized[n], "Merging commands", getPattern(optimized[n]))
 				}
 			}
 			optimized = append(optimized, commands[i])
 			cnt++
-			if !initialValueChanged {
-				initialValueChanged = strings.Contains("+-,", commands[i].String)
-			}
-			if commands[i].String == "[" {
-				currentLoop = []*Command{commands[i]}
-				currentLoopBegin = cnt
-			} else if len(currentLoop) > 0 {
-				currentLoop = append(currentLoop, commands[i])
-				if commands[i].String == "]" {
-					if isMultiplyLoop(currentLoop) {
-						cnt = currentLoopBegin
-						optimized = optimized[:cnt+1]
-						optimized[cnt].String = getLoopPattern(currentLoop)
-						optimized[cnt].MultiplyLoop = newMultiplyLoop(currentLoop)
-						report(verbose, currentLoop[0], "Optimizing loop", optimized[cnt].String)
-					}
-					currentLoop = []*Command{}
-				}
-			}
-			i++
 		}
+		i++
 	}
+	return optimized
+}
 
+func optimizeMultiplyLoops(commands []*Command, verbose bool) []*Command {
+	optimized := []*Command{}
+	cnt := -1
+	i := 0
+	currentLoop := []*Command{}
+	currentLoopBegin := 0
+	for i < len(commands) {
+		optimized = append(optimized, commands[i])
+		cnt++
+		if commands[i].String == "[" {
+			currentLoop = []*Command{commands[i]}
+			currentLoopBegin = cnt
+		} else if len(currentLoop) > 0 {
+			currentLoop = append(currentLoop, commands[i])
+			if commands[i].String == "]" {
+				if isMultiplyLoop(currentLoop) {
+					cnt = currentLoopBegin
+					optimized = optimized[:cnt+1]
+					optimized[cnt].String = getLoopPattern(currentLoop)
+					optimized[cnt].MultiplyLoop = newMultiplyLoop(currentLoop)
+					report(verbose, currentLoop[0], "Optimizing loop", optimized[cnt].String)
+				}
+				currentLoop = []*Command{}
+			}
+		}
+		i++
+	}
 	return optimized
 }
 
